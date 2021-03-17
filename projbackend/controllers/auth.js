a0a18a002defa4e0cd48ce951b660b94d5eb105f
Akash <as55848@gmail.com>
2021-03-17 18:42:42 +0530

    user signup and sign in using token

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,21 @@
 const User = require("../models/user");
+const { check, validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expJwt = require('express-jwt');
+
 
 exports.signup = (req,res) => {
+
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: 
+        {
+            message:errors.array()[0].msg,
+            param:errors.array()[0].param
+        }
+    });     //error 422 when error coming from backend or database
+  }
+
    const user = new User(req.body)
    user.save((err, user) => {
     if(err){
@@ -13,8 +28,47 @@ exports.signup = (req,res) => {
         email: user.email,
         id : user._id
     });
-   })
-}
+   });
+};
+
+exports.signin = (req, res) => {
+    const {email, password} = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: 
+            {
+                message:errors.array()[0].msg,
+                param:errors.array()[0].param
+            }
+        });
+    }
+
+    User.findOne({email}, (err,user) => {
+        if(err || !user){
+            res.status(400).json({
+                error: "User email does not exits"
+            })
+        }
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and password do not match"
+            })
+        }
+
+        //create token
+        const token = jwt.sign({_id: user._id}, process.env.SECRET)
+
+        //put token in cookie
+        res.cookie("token",token, {expire: new Date() + 9999});
+
+        //send response to front end
+        const {_id, name , email,role} = user;
+        return res.json({
+            token,user: {_id,name,email,role}
+        });
+
+    });
+};
 
 exports.signout = (req, res) => {
     res.json({
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,12 +1,27 @@
 var express = require('express')
 var router = express.Router()
-const {signup, signout}  = require("../controllers/auth")
+const { check, validationResult } = require('express-validator');
+const {signup, signout,signin}  = require("../controllers/auth");
+
 
 // const signout = (req, res) => {
 //     res.send("user signout success")
 // };
 
-router.post("/signup", signup);
+//data validation in routes
+router.post("/signup",
+check('name').isLength({ min: 3 }).withMessage('name must be at least 3 chars long'),
+check('email').isEmail().withMessage('email required'),
+check('password').isLength({min:3}).withMessage('password should be atleast 3 char'),
+signup
+);
+
+router.post("/signin",
+check('email').isEmail().withMessage('email is required'),
+check('password').isLength({min:3}).withMessage('password field is required'),
+signin
+);
+
 router.get("/signout", signout);
 
 module.exports = router;
